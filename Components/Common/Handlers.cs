10f53b5e0d8a65a9282ec8a982d36df3154128a6
cbdix92 <cbdix92@gmail.com>
2020-06-24 22:42:12 -0400

    Handler prototype added

---
+++ Components/Common/Handlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CMDR.Components
+{
+    public class Component<T>
+        where T: IComponent<T>
+    {
+        public readonly Type Type;
+        public readonly Scene Scene;
+        internal T Data;
+        public T Get
+        {
+            get
+            {
+                // There has to be a better way to do this without needing to retrieve the entire collection. Something to look into later.
+                return Scene.Components.Get<T>()[Data.ID];
+            }
+        }
+    }
+
+    public class GameObject
+    {
+        public readonly Scene Scene;
+        internal SGameObject Data;
+        public SGameObject Get
+        {
+            get
+            {
+                return Scene.GameObjects[Data.ID];
+            }
+        }
+    }
+}
