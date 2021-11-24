574c72e79022d1a32e78aa948b622ab91b4b257e
cbdix92 <cbdix92@gmail.com>
2021-11-24 18:37:35 -0500

    Update

--- Systems/Render.cs
+++ Systems/Render.cs
@@ -10,39 +10,23 @@ namespace CMDR.Systems
     internal static class Render
     {
         internal static Scene Scene { get => SceneManager.ActiveScene; }
-
-        public static byte ZDepth;
-
-        public static Matrix4 ProjectionMatrix = Matrix4.CreatePerspectiveFOV((float)Math.PI / 3, Camera.Width / Camera.Height, 0.1f, 2000);
-        public static Matrix4 CameraMatrix = Matrix4.CreateTranslation(Camera.X, Camera.Y, Camera.Z);
-
-
-        internal static uint VAO;
-        internal static uint VBO;
+		
+		
+		public static Matrix4 ProjectionMatrix = Matrix4.CreateOrthographic();
+		
         internal static void ClearScreen()
         {
             GL.Clear(BUFFER_MASK.COLOR_BUFFER_BIT | BUFFER_MASK.DEPTH_BUFFER_BIT);
         }
+		
         internal static void ScreenBuffer(long ticks)
         {
-
-            
-
 
             Debugger.Draw(ticks);
 
             Transform[] transforms = Scene.Components.Get<Transform>();
             RenderData[] renderables = Scene.Components.Get<RenderData>();
             IEnumerable<SGameObject> gameObjects = Camera.GetRenderable(transforms);
-
-            (float camX,float camY) = (Camera.X, Camera.Y);
-
-            //VAO = GL.GenVertexArray();
-            //VBO = GL.GenBuffer();
-			//GL.BindBuffer(GL.BUFFER_BINDING_TARGET.ARRAY_BUFFER, VBO);
-            
-			
-			//GL.BindVertexArray(VAO);
 
             foreach(SGameObject gameObject in Camera.GetRenderable(transforms))
             {
@@ -57,7 +41,6 @@ namespace CMDR.Systems
 
                 Debugger.DrawBoundingBox(gameObject);
             }
-            Glfw.SwapBuffers(Display.Window);
         }
         internal static void Update(long ticks)
         {
@@ -65,7 +48,7 @@ namespace CMDR.Systems
             // Draw images to internal buffer
             ScreenBuffer(ticks);
             // Draw images to the screen
-            //Buffer.Render();
+            Glfw.SwapBuffers(Display.Window);
         }
     }
 }
